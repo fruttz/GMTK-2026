5f4d52f9b6c11590bd6004eccfdb9abcfc55b5d6
Dito <dito.arjwasa@gmail.com>
2026-07-24 22:57:05 +0700

    the word game logic is done

--- dictionautica/Scenes/letters.gd
+++ dictionautica/Scenes/letters.gd
@@ -1,25 +1,27 @@
 extends Node2D
 
-var possible_letters  = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var letter
 var value
 var sprite
 var dragging = false
-var click_radius = 120
+var click_radius = 64
 
 func init(l : String):
-	if l.to_upper() in possible_letters:
-		letter = l.to_upper()
-		value = 1
-		$Sprite/Label.text = letter
+	letter = l.to_upper()
+	value = 1
+	$Sprite/Letter.text = letter
+	$Sprite/Value.text = str(value)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if (event.position - $Sprite.position).length() < click_radius:
+		if (event.position - position).length() < click_radius:
 			if not dragging and event.is_pressed():
 				dragging = true
+				self.freeze = true
 		if dragging and not event.is_pressed():
 			dragging = false
+			self.freeze = false
 	if event is InputEventMouseMotion and dragging:
-		$Sprite.position = event.position
+		position = event.position
+	
 	
---
+++ dictionautica/Scenes/w_container.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+var bodies = []
+
+
+func _on_area_2d_body_shape_entered(_body_rid: RID, body: Node2D, _body_shape_index: int, _local_shape_index: int) -> void:
+	bodies.append(body)
+
+func _on_area_2d_body_shape_exited(_body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body != null:
+		var removed_instance_id = body.get_instance_id()
+		for b in bodies:
+			if b.get_instance_id() == removed_instance_id:
+				bodies.erase(b)
+
+func compare_dict():
+	var file = FileAccess.open("res://Scripts/dictionary.txt", FileAccess.READ)
+	var content = file.get_as_text().split("\n")
+	return content
+
+func read_letters():
+	var letters = []
+	bodies.sort_custom(func(b1, b2): return b1.position.x < b2.position.x)
+	for b in bodies:
+		letters.append(b.letter)
+	var word = "".join(letters)
+	return word
+	
+func _on_submit_button_down() -> void:
+	if read_letters() in compare_dict():
+		print(read_letters())
+		for b in bodies:
+			b.queue_free()
+	else:
+		print("Not a Word!")
+		
+		
+	
