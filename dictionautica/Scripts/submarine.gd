852f9dfc0b7e1935531523038d59a86d64a74577
RubiMugmug <mugmugcomedy@gmail.com>
2026-07-24 21:58:53 +0200

    Die and Respawn
    Multiple Flags and functions to control for failstate.
    Beware the debug unalive buttons.

--- dictionautica/Scripts/submarine.gd
+++ dictionautica/Scripts/submarine.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Node2D
 
 var velocity = Vector2(0,0)
 var omega : float = 0
@@ -12,40 +12,44 @@ var harpoon_range_tier : float = 2
 var omega_speed_tier : float = 0.5
 
 var catching : bool = true
+var stashing : bool = true
+var alive: bool = true
 
 var TOPLEFT = get
 
 func _physics_process(delta: float) -> void:
 	#Character Polling Inputs
-	if Input.is_action_pressed("move_up"):
-		velocity += Vector2(0,-100)*delta*speed_tier
-	if Input.is_action_pressed("move_down"):
-		velocity += Vector2(0,+100)*delta*speed_tier
-	if Input.is_action_pressed("move_left"):
-		velocity += Vector2(-100,0)*delta*speed_tier
-	if Input.is_action_pressed("move_right"):
-		velocity += Vector2(+100,0)*delta*speed_tier
-	if Input.is_action_pressed("rotate_plus"):
-		omega += 5*delta
-	if Input.is_action_pressed("rotate_minus"):
-		omega -= 5*delta
-		
+	if alive:
+		if Input.is_action_pressed("move_up"):
+			velocity += Vector2(0,-100)*delta*speed_tier
+		if Input.is_action_pressed("move_down"):
+			velocity += Vector2(0,+100)*delta*speed_tier
+		if Input.is_action_pressed("move_left"):
+			velocity += Vector2(-100,0)*delta*speed_tier
+		if Input.is_action_pressed("move_right"):
+			velocity += Vector2(+100,0)*delta*speed_tier
+		if Input.is_action_pressed("rotate_plus"):
+			omega += 5*delta
+		if Input.is_action_pressed("rotate_minus"):
+			omega -= 5*delta
+	else:
+		velocity = Vector2(0,-300)
 	#Character Physics
 	position += delta*velocity
 	velocity = velocity/(exp(delta))
-	rotation += delta*omega*omega_speed_tier
+	$SubmarineSprite.rotation += delta*omega*omega_speed_tier
 	omega = omega/(exp(delta))
 
 	#Harpoon Control
-	if Input.is_action_pressed("launch_harpoon"):
+	if alive and Input.is_action_pressed("launch_harpoon"):
 		harpoon_velocity = Vector2(100,0)
 	else:
 		harpoon_velocity = Vector2(-100,0)
-	$Harpoon.position += harpoon_velocity*delta*harpoon_speed_tier
-	if $Harpoon.position.x > harpoon_range*harpoon_range_tier:
-		$Harpoon.position = Vector2(harpoon_range*harpoon_range_tier,0)
-	if 0 > $Harpoon.position.x :
-		$Harpoon.position = Vector2(0,0)
+	$SubmarineSprite/Harpoon.position += harpoon_velocity*delta*harpoon_speed_tier
+	if $SubmarineSprite/Harpoon.position.x > harpoon_range*harpoon_range_tier:
+		$SubmarineSprite/Harpoon.position = Vector2(harpoon_range*harpoon_range_tier,0)
+	if 0 > $SubmarineSprite/Harpoon.position.x :
+		$SubmarineSprite/Harpoon.position = Vector2(0,0)
 		store_fish()
 
 func store_fish():
@@ -54,15 +58,25 @@ func store_fish():
 
 func _on_harpoon_body_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if catching:
-		body.get_parent().catch($Harpoon)
+		body.get_parent().catch($SubmarineSprite/Harpoon)
 		catching = false
 	#reparent ke Harpoon 
 	
 func _on_door_body_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	get_parent().store(body.get_parent())
-	catching = true
+	if stashing:
+		get_parent().store(body.get_parent())
+		catching = true
 
+func die():
+	stashing = false
+	catching = false
+	alive = false
 	
+func respawn():
+	velocity = Vector2(0,0)
+	stashing = true
+	catching = true
+	alive = true
 #func _input(event: InputEvent) -> void:
 	##Character Event Inputs
 	#if event.is_action_pressed("launch_harpoon"):
